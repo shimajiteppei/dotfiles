566a5999df65f31b3d82d20c9796e3503f0ba229
tomoesaturn <hotarutwtiiter@gmail.com>
2023-08-29 15:01:06 +0900

    allow long branch name for p10k

--- .p10k.zsh
+++ .p10k.zsh
@@ -387,12 +387,14 @@
 
     local res
 
+    typeset -g __VCS_SHORTEN_LEN=256
+
     if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
       local branch=${(V)VCS_STATUS_LOCAL_BRANCH}
       # If local branch name is at most 32 characters long, show it in full.
       # Otherwise show the first 12 … the last 12.
       # Tip: To always show local branch name in full without truncation, delete the next line.
-      (( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
+      (( $#branch > $__VCS_SHORTEN_LEN )) && branch[13,-13]="…"  # <-- this line
       res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
     fi
 
@@ -405,7 +407,7 @@
       # If tag name is at most 32 characters long, show it in full.
       # Otherwise show the first 12 … the last 12.
       # Tip: To always show tag name in full without truncation, delete the next line.
-      (( $#tag > 32 )) && tag[13,-13]="…"  # <-- this line
+      (( $#tag > $__VCS_SHORTEN_LEN )) && tag[13,-13]="…"  # <-- this line
       res+="${meta}#${clean}${tag//\%/%%}"
     fi
 
