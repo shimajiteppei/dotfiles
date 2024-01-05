a6d77cfdf83c2eaf5cb1f192fb2deb17650c4491
tomoesaturn <hotarutwitter@gmail.com>
2024-01-05 11:43:20 +0900

    revive provisioning scripts

---
+++ .local/share/v4l2loopback/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# https://github.com/umlaeute/v4l2loopback#distributions
+# sudo apt install v4l2loopback-source module-assistant
+sudo module-assistant auto-install v4l2loopback-source
