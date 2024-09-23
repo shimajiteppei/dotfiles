32eef6f0a457fbdd2f6a243f7396ef7b8f315aa8
shimajiteppei <hotarutwitter@gmail.com>
2024-09-23 22:09:45 +0900

    noninteractive rust install

--- .config/zsh/widget/rust.zsh
+++ .config/zsh/widget/rust.zsh
@@ -13,7 +13,7 @@ export PATH
 ## install
 ##
 
-command -v cargo >/dev/null || (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh)
+command -v cargo >/dev/null || (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y)
 command -v cargo-install-update >/dev/null || cargo install cargo-update
 
 
