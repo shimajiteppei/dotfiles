6416a0968a071ae043e6672199aba1d1df095f19
shimajiteppei <hotarutwitter@gmail.com>
2024-10-12 20:00:02 +0900

    remove v4l2loopback script

--- .local/share/v4l2loopback/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-# https://github.com/umlaeute/v4l2loopback#distributions
-# sudo apt install v4l2loopback-source module-assistant
-sudo module-assistant auto-install v4l2loopback-source
