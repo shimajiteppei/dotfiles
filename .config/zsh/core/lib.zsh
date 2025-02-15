53f19bad044913645a1bf3533e4f001a1e96ddbd
shimajiteppei <hotarutwitter@gmail.com>
2025-02-15 17:35:44 +0900

    refine theme, abbr script

--- .config/zsh/core/lib.zsh
+++ .config/zsh/core/lib.zsh
@@ -1,15 +1,37 @@
 __dotfiles_core-init() {
-    source $ZDOTDIR/core/zinit.zsh
+    ##
+    ## init zinit
+    ##
+    source "$XDG_DATA_HOME/zinit/zinit.git/zinit.zsh"
+    autoload -Uz _zinit
+    (( ${+_comps} )) && _comps[zinit]=_zinit
+    zinit light-mode for \
+        romkatv/powerlevel10k \
+        zdharma-continuum/history-search-multi-word \
+        zdharma-continuum/fast-syntax-highlighting \
+        zsh-users/zsh-completions \
+        olets/zsh-abbr
+
+    ##
+    ## load scripts
+    ## do not use p10k instant prompt
+    ##
+    source $ZDOTDIR/core/p10k.zsh
     source $ZDOTDIR/core/history.zsh
     source $ZDOTDIR/core/completion.zsh
-    source $ZDOTDIR/core/alias.zsh
 }
 
 __dotfiles_core-update() {
+    ##
+    ## update source
+    ##
     cd $HOME
     git submodule update --recursive
     cd -
 
+    ##
+    ## update plugins
+    ##
     zinit self-update
     zinit update
 }
--- .config/zsh/widget/abbr.zsh
+++ .config/zsh/widget/abbr.zsh
@@ -6,7 +6,56 @@ __DOTFILES_WIDGET_NAME=abbr
 ##
 "__dotfiles_widget-init-${__DOTFILES_WIDGET_NAME}"() {
     ##
-    ## alias
+    ## init abbr
+    ##
+    abbr clear-session
+    # there may have been a breaking change in v6
+    local __ABBR_MAJOR_VERSION=$(abbr --version | awk '{print $3}' | awk -F. '{print $1}')
+    if [[ $__ABBR_MAJOR_VERSION -ge 6 ]]; then
+        alias() {
+            abbr --session --regular --quieter --force $@
+            abbr --session --global --quieter --force $@
+        }
+    else
+        alias() {
+            abbr --session --global --quieter --force $@
+        }
+    fi
+    unset __ABBR_MAJOR_VERSION
+
+
+    ##
+    ## set alias
+    ##
+    alias history="history 0"
+    alias grep='grep --color=auto'
+    alias ll='ls -lahF --color'
+    alias cp='cp -i'
+    alias mv='mv -i'
+    alias rm='rm -i'
+    alias mkdir='mkdir -p'
+
+    alias g='git'
+    # to avoid running ghostscript by type error
+    alias gs='git status --short'
+    # https://zsh-abbr.olets.dev/commands.html#git
+    alias 'git s'='git status --short'
+    alias 'git ap'='git add -p'
+    alias 'git b'='git branch'
+    alias 'git cm'='git commit -m'
+    alias 'git ch'='git checkout'
+    alias 'git re'='git reset'
+    # disable switch/restore
+    alias 'git switch'='git checkout'
+    alias 'git restore'='git reset'
+    # override
+    alias 'git clone'='git clone --recurse-submodules'
+    alias 'git config'='git config --local'
+    alias 'git pull'='git fetch --all --tags --prune --prune-tags && git pull --rebase --autostash'
+
+
+    ##
+    ## add highlight settings
     ##
     # fast-syntax-highlighting highlighting of abbreviations (https://github.com/olets/zsh-abbr/issues/24)
     chroma_single_word() {
