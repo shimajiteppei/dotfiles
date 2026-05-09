556e38912adb569287a7cfbcf7a0f0a8f3a2d213
shimajiteppei <shimajiteppei@gmail.com>
2026-05-09 20:10:16 +0900

    output installer log file when err

--- workspace/dotfiles/installer.mac.sh
+++ workspace/dotfiles/installer.mac.sh
@@ -3,8 +3,16 @@
 brew install curl git cmake coreutils bash
 
 cd $(dirname $0)
-if ./installer.sh; then
+
+EXIT_CODE_FILE="installer_result"
+LOG_FILE="installer.log"
+( ./installer.sh; echo $? >&3 ) 3>$EXIT_CODE_FILE 2>&1 | tee "$LOG_FILE"
+EXIT_CODE=$(cat $EXIT_CODE_FILE)
+rm "$EXIT_CODE_FILE"
+
+if test ${EXIT_CODE:-1} -eq 0; then
     echo 'installation success'
+    exit 0
 else
     echo 'installation failed'
     exit 1
